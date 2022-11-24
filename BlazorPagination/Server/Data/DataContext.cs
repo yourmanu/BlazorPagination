948d5e7ea4094c1300807ef7d296def1269b07b2
Sivakumar G Nair <Sivakumar.Nair@wilhelmsen.com>
2022-11-24 14:40:38 +0530

    Add project files.

---
+++ BlazorPagination/Server/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using BlazorPagination.Shared;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorPagination.Server.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Car> Cars => Set<Car>();
+    }
+}
---
+++ BlazorPagination/Shared/Car.cs
@@ -0,0 +1,11 @@
+﻿namespace BlazorPagination.Shared
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string? Make { get; set; }
+        public string? Model { get; set; }
+        public int Year { get; set; }
+        public int Hp { get; set; }
+    }
+}
