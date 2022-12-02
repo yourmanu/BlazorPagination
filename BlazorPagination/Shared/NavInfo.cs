31db4fd5dddaf356cb85308af5c260f165369b5a
Sivakumar G Nair <Sivakumar.Nair@wilhelmsen.com>
2022-12-02 13:13:21 +0530

    show car specs on click

---
+++ BlazorPagination/Shared/NavInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorPagination.Shared
+{
+    public class NavInfo
+    {
+        public int Id { get; set; }
+        public int SourcePageNo { get; set; }
+    }
+}
